fd9e8acf9a3079291cf200997f184a7b659558b2
Stanislav Levental <stanislav.levental@grammarly.com>
2024-01-27 20:40:16 -0500

    Tokenizer and stemmer added

--- src/emb25/utils.rs
+++ src/emb25/utils.rs
@@ -2,17 +2,26 @@ use tantivy::tokenizer::*;
 
 
 pub fn tokenize(text: &str) -> Vec<String> {
-    let en_stem = TextAnalyzer::builder(SimpleTokenizer::default())
+    let mut en_stem = TextAnalyzer::builder(SimpleTokenizer::default())
         .filter(RemoveLongFilter::limit(40))
-        .filter(LowerCaser)
         .filter(Stemmer::new(Language::English))
         .build();
-
     let mut tokens = Vec::new();
+    en_stem.token_stream(text).process(&mut |token| {
+        tokens.push(token.text.clone());
+    });
 
-
-    for word in text.split_whitespace() {
-        tokens.push(word.to_string());
-    }
     tokens
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_tokenize() {
+        let text = "This is a test";
+        let tokens = tokenize(text);
+        assert_eq!(tokens, vec!["This", "is", "a", "test"]);
+    }
 }
